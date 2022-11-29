7731f08d88a6ebdc56e9459184f095ae5cbac8d4
Ariana Camargos <arianacamargos16@gmail.com>
2022-11-29 20:08:52 -0300

    feat: insere arquivos

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const DioPages());
+}
+
+class DioPages extends StatelessWidget {
+  const DioPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
